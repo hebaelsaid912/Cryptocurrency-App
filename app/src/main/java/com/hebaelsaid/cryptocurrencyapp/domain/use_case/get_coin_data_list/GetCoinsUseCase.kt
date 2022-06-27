0e3cc5d06e3851ef66c09444a09ab0a492dc4a3d
Heba Elsaid <72816466+hebaelsaid912@users.noreply.github.com>
2022-06-27 15:23:31 +0200

    add use cases

---
+++ app/src/main/java/com/hebaelsaid/cryptocurrencyapp/domain/use_case/get_coin_data_list/GetCoinsUseCase.kt
@@ -0,0 +1,28 @@
+package com.hebaelsaid.cryptocurrencyapp.domain.use_case.get_coin_data_list
+
+import com.hebaelsaid.cryptocurrencyapp.common.Resource
+import com.hebaelsaid.cryptocurrencyapp.data.remote.dto.coins.toCoin
+import com.hebaelsaid.cryptocurrencyapp.domain.model.Coin
+import com.hebaelsaid.cryptocurrencyapp.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import java.util.concurrent.Flow
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(
+    private val repository: CoinRepository
+){
+    operator fun invoke() = flow<Resource<List<Coin>>> {
+        try {
+            emit(Resource.Loading())
+            val coin = repository.getCoins().map { it.toCoin() }
+            emit(Resource.Success(coin))
+        }catch (e: HttpException){
+            emit(Resource.Error(e.localizedMessage?: "An unexpected error occurred"))
+        }catch (e: IOException){
+            emit(Resource.Error("Couldn't reach server. Please check your internet connection"))
+        }
+    }
+
+}
---
+++ app/src/main/java/com/hebaelsaid/cryptocurrencyapp/domain/use_case/get_coin_detail/GetCoinDetailsUseCase.kt
@@ -0,0 +1,30 @@
+package com.hebaelsaid.cryptocurrencyapp.domain.use_case.get_coin_detail
+
+import com.hebaelsaid.cryptocurrencyapp.common.Resource
+import com.hebaelsaid.cryptocurrencyapp.data.remote.dto.coin_detail.toCoinDetail
+import com.hebaelsaid.cryptocurrencyapp.data.remote.dto.coins.toCoin
+import com.hebaelsaid.cryptocurrencyapp.domain.model.Coin
+import com.hebaelsaid.cryptocurrencyapp.domain.model.CoinDetail
+import com.hebaelsaid.cryptocurrencyapp.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import java.util.concurrent.Flow
+import javax.inject.Inject
+
+class GetCoinDetailsUseCase @Inject constructor(
+    private val repository: CoinRepository
+){
+    operator fun invoke(coinId:String) = flow<Resource<CoinDetail>> {
+        try {
+            emit(Resource.Loading())
+            val coinDetails = repository.getCoinById(coinId = coinId).toCoinDetail()
+            emit(Resource.Success(coinDetails))
+        }catch (e: HttpException){
+            emit(Resource.Error(e.localizedMessage?: "An unexpected error occurred"))
+        }catch (e: IOException){
+            emit(Resource.Error("Couldn't reach server. Please check your internet connection"))
+        }
+    }
+
+}
